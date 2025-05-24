af282e098603467e5f9e5a6b9903efd429d0ebe0
retailsuccesscoach <laurenpercival@thebloommarketing.com>
2025-05-24 11:22:04 +0100

    Render Cal.com calendar embed in profit-performance-call page

--- src/app/profit-performance-call/page.tsx
+++ src/app/profit-performance-call/page.tsx
@@ -1,7 +1,36 @@
-// src/app/profit-performance-call/page.tsx
+'use client';
 
-import CalComPage from './calcompage';
+import { useEffect } from 'react';
 
 export default function ProfitPerformanceCallPage() {
-  return <CalComPage />;
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = "https://app.cal.com/embed/embed.js";
+    script.async = true;
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      if (window.Cal) {
+        window.Cal("init", "45-min-meeting", { origin: "https://cal.com" });
+        window.Cal.ns["45-min-meeting"]("inline", {
+          elementOrSelector: "#my-cal-inline",
+          config: { layout: "month_view" },
+          calLink: "lauren-percival-xz8hq3/45-min-meeting",
+        });
+        window.Cal.ns["45-min-meeting"]("ui", { hideEventTypeDetails: false, layout: "month_view" });
+      }
+    };
+
+    return () => {
+      const calScript = document.querySelector('script[src="https://app.cal.com/embed/embed.js"]');
+      if (calScript) document.head.removeChild(calScript);
+    };
+  }, []);
+
+  return (
+    <div
+      id="my-cal-inline"
+      style={{ width: '100%', height: '700px', overflow: 'auto' }}
+    />
+  );
 }
